ec9ec016428baff5ef484237cc40f1a6cfba118e
Shandy Sulen <shandysulen@ufl.edu>
2017-11-04 22:01:47 -0400

    Connected front and back end with HTTP post service

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -1,11 +1,13 @@
 export class MainController {
-  constructor () {
+	
+  constructor ($http) {
  		'ngInject';
-		
+		this.$http = $http;
   }
 	
-		postMessage() {
-			console.log("post");
-		}
+	//Use Angular's HTTP service to post data to the follow URL
+	postMessage() {
+		this.$http.post('http://localhost:5000/api/message', {msg: this.message});
+	}
 
 }
